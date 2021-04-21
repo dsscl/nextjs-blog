9ff83de78c2e3886df7e66246b1f3dc810af8cd5
陈雅凯 <chenyakai@chenyakaideMacBook-Pro.local>
2021-04-21 17:13:46 +0800

    js转ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,10 @@ export default function Home({
   allPostsData
 } : {
   allPostsData: {
-    date: string,
-    title: string,
+    date: string
+    title: string
     id: string
-  }
+  }[]
 }) {
   return (
     <Layout home>
